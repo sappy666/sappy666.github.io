254f5887a39677569f19e28083fc0337b628f2a6
Sappy <macarenaramdohr@gmail.com>
2022-12-06 14:24:10 -0300

    arreglos navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
+$(function(){
     $(".destacados-fade").click(function(){
         $(".cartas").slideToggle("normal");
+    });
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 700){
+            $(".navbar").addClass("bg-white");
+        }
+        if($(this).scrollTop() < 700){
+            $(".navbar").removeClass("bg-white");
+        }
     });
 });
