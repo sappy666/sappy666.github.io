a0c7932284f73cb21e9a546dbd2d604f85c8e922
Sappy <macarenaramdohr@gmail.com>
2022-12-06 20:19:19 -0300

    agrego tooltips y comentarios

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,17 @@
 $(function(){
+    //---Esconde seccion destacados
     $(".destacados-fade").click(function(){
-        $(".cartas").slideToggle("normal");
+        $(".cartas").slideToggle("slow");
     });
     $(window).scroll(function(){
+        //---cuando navbar esta sobre imagenes de carrusel
         if ($(this).scrollTop() > $(".header").height()-100){
             $(".navbar").addClass("bg-white");
             $(".navbar").removeClass("bg-transparent");
             $(".navbar").removeClass("navbar-dark");
             
         }
+        //---Cuando navbar sale de las imagenes de carrusel
         if($(this).scrollTop() < $(".header").height()-100){
             $(".navbar").removeClass("bg-white");
             $(".navbar").addClass("bg-transparent");
@@ -16,3 +19,8 @@ $(function(){
         }
     });
 });
+//---inicia tooltip en toda la pagina---
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
