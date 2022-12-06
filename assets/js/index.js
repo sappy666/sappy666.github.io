e1c0188f999e88d31616090bef2593566527a87c
Sappy <macarenaramdohr@gmail.com>
2022-12-06 19:53:10 -0300

    arreglo iconos quienes somos

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,16 @@ $(function(){
         $(".cartas").slideToggle("normal");
     });
     $(window).scroll(function(){
-        if ($(this).scrollTop() > 700){
+        if ($(this).scrollTop() > $(".header").height()-100){
             $(".navbar").addClass("bg-white");
+            $(".navbar").removeClass("bg-transparent");
+            $(".navbar").removeClass("navbar-dark");
+            
         }
-        if($(this).scrollTop() < 700){
+        if($(this).scrollTop() < $(".header").height()-100){
             $(".navbar").removeClass("bg-white");
+            $(".navbar").addClass("bg-transparent");
+            $(".navbar").addClass("navbar-dark");
         }
     });
 });
